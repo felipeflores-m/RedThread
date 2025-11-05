d0052989243566385dba4a751beaace3ec5613c6
Felipe Flores <felipeandresfloresmoya@gmail.com>
2025-11-05 09:23:37 -0300

    commit inicial

---
+++ indetity-service/src/main/java/com/redthread/indetity/IndetityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.redthread.indetity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndetityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndetityServiceApplication.class, args);
+	}
+
+}
